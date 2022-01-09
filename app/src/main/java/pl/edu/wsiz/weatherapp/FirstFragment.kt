fe25f8836bbadac79dcf95a9be4efcb1a24f45a8
Marcin Lipa <marcin.lipa@ttsw.com.pl>
2022-01-09 18:55:44 +0100

    display api response

--- app/src/main/java/pl/edu/wsiz/weatherapp/FirstFragment.kt
+++ app/src/main/java/pl/edu/wsiz/weatherapp/FirstFragment.kt
@@ -2,6 +2,7 @@ package pl.edu.wsiz.weatherapp
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
@@ -9,21 +10,36 @@ import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.google.android.material.textfield.TextInputEditText
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.singleton
 import pl.edu.wsiz.weatherapp.databinding.FragmentFirstBinding
+import pl.edu.wsiz.weatherapp.network.ApiInterface
+import pl.edu.wsiz.weatherapp.network.ForecastDTO
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+const val WEATHER_UNIT = "metric"
+
+class FirstFragment() : Fragment(), KodeinAware {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private val API_KEY = "31b0e4e921035c8148e5c6ceb7ef4cce"
+
+
+    override val kodein = Kodein.lazy {
+
+        bind() from singleton { ApiInterface() }
+
+    }
+    private val api: ApiInterface by instance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,16 +55,11 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val cityInput = view.findViewById<TextInputEditText>(R.id.city)
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
         cityInput.setOnEditorActionListener { _, keyCode, event ->
             if (((event?.action ?: -1) == KeyEvent.ACTION_DOWN)
                 || keyCode == EditorInfo.IME_ACTION_DONE
             ) {
-
-                val findViewById = view.findViewById<TextView>(R.id.textview_first)
-                findViewById.text = "${cityInput.text}"
+                getCurrentWeather(cityInput.text.toString(), view)
                 view.hideSoftInput()
 
                 return@setOnEditorActionListener true
@@ -61,6 +72,26 @@ class FirstFragment : Fragment() {
         val inputMethodManager =
             context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+    }
+
+    fun getCurrentWeather(city: String, view: View) {
+        val currentWeather = api.getCurrentWeather(city, WEATHER_UNIT, API_KEY)
+        var result: ForecastDTO? = null
+        currentWeather.enqueue(object : Callback<ForecastDTO?> {
+            override fun onResponse(call: Call<ForecastDTO?>, response: Response<ForecastDTO?>) {
+                result = response.body()
+                if (result == null) {
+                    Toast.makeText(context, "Nie znaleziono miasta", Toast.LENGTH_LONG).show()
+                } else {
+                    val findViewById = view.findViewById<TextView>(R.id.textview_first)
+                    findViewById.text = "${result!!.main.feels_like}"
+                }
+            }
+
+            override fun onFailure(call: Call<ForecastDTO?>, t: Throwable) {
+                Log.d("apiCall", "onFailure: ${t.message}")
+            }
+        })
     }
 
     override fun onDestroyView() {
